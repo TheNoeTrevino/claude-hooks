388d8a7c273237d55b94bfaa16a77292f7c6d1dc
TheNoeTrevino <noetrevino.music@gmail.com>
2025-12-17 06:42:48 -0600

    update: fix timeout

--- answer-question.sh
+++ answer-question.sh
@@ -3,7 +3,7 @@
 if [ -n "$TMUX" ]; then
   # get the tmux session name
   tmux_session=$(tmux display-message -p '#S')
-  notify-send --urgency=critical 'Claude Question' "Asking question in session: $tmux_session" -t 3000
+  notify-send 'Claude Question' "Asking question in session: $tmux_session" -t 3000
 else
-  notify-send --urgency=critical 'Claude Question' "Asking you a question" -t 3000
+  notify-send 'Claude Question' "Asking you a question" -t 3000
 fi
--- needs-permissions.sh
+++ needs-permissions.sh
@@ -3,7 +3,7 @@
 if [ -n "$TMUX" ]; then
   # get the tmux session name
   tmux_session=$(tmux display-message -p '#S')
-  notify-send --urgency=critical 'Claude Edit' "Requesting user input in session: $tmux_session" -t 3000
+  notify-send 'Claude Edit' "Requesting user input in session: $tmux_session" -t 3000
 else
-  notify-send --urgency=critical 'Claude Edit' "Requesting user input" -t 3000
+  notify-send 'Claude Edit' "Requesting user input" -t 3000
 fi
